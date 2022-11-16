da71f5facc79f32b18f5e2934ad144f0531c7635
Berret Terry <berretterry@gmail.com>
2022-11-16 13:43:26 -0800

    more adjustments from plan

--- bastion.tf
+++ bastion.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "eks_bastion" {
     ami = "ami-04a616933df665b44"
     instance_type = "t2.micro"
     security_groups = aws_security_group.worker_group.id
-    subnet_ids = element(aws_subnet.public_subnet.*.id,count.index)
+    subnet_id = aws_subnet.public_subnet.0.id
     associate_public_ip_address = true
     tags = {
         "Name" = "EKS-Bastion"
@@ -13,16 +13,18 @@ resource "aws_instance" "eks_bastion" {
     provisioner "file" {
         source = "eks_bastion_ssh.pem"
         destination = "/home/ec2-user/eks_bastion_ssh.pem"
-        connection = {
+        
+        connection {
             type = "ssh"
             user = "ec2-user"
             private_key = file("eks_bastion_ssh.pem")
-            host = self.map_public_ip
+            host = self.public_ip
 
         }
     }
     provisioner "remote-exec" {
         inline = ["chmod 400 ~/eks_bastion_ssh.pem"]
+        
         connection {
             type = "ssh"
             user = "ec2-user"
--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "learn_eks_cluster" {
     role_arn = aws_iam_role.eks-iam-role.arn
 
     vpc_config {
-      subnet_ids = element(aws_subnet.public_subnet.*.id,count.index)
+      subnet_ids = aws_subnet.public_subnet.0.id
     }
     
     depends_on = [
@@ -15,7 +15,7 @@ resource "aws_eks_node_group" "worker_node_group" {
     cluster_name = aws_eks_cluster.learn_eks_cluster.name
     node_group_name = "learn_eks_workernodes"
     node_role_arn = aws_iam_role.workernodes.arn
-    subnet_ids = element(aws_subnet.public_subnet.*.id,count.index)
+    subnet_ids = aws_subnet.public_subnet.0.id
     instance_types = ["t3.micro"]
 
     scaling_config {
