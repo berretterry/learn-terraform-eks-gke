6b210ca5ae34f2b82bec7443d4d4273d596c0316
Berret Terry <berretterry@gmail.com>
2022-11-16 12:51:09 -0800

    updated files per plan output

--- bastion.tf
+++ bastion.tf
@@ -2,34 +2,34 @@ resource "aws_instance" "eks_bastion" {
     ami = "ami-04a616933df665b44"
     instance_type = "t2.micro"
     security_groups = aws_security_group.worker_group.id
-    subnet_id = element(aws_subnet.public_subnet.*.id,count.index)
+    subnet_ids = element(aws_subnet.public_subnet.*.id,count.index)
     associate_public_ip_address = true
     tags = {
         "Name" = "EKS-Bastion"
     }
   
-}
 
-provisioner "file" {
-    source = "eks_bastion_ssh.pem"
-    destination = "/home/ec2-user/eks_bastion_ssh.pem"
-    connection = {
-        type = "ssh"
-        user = "ec2-user"
-        private_key = file("eks_bastion_ssh.pem")
-        host = self.map_public_ip
+
+    provisioner "file" {
+        source = "eks_bastion_ssh.pem"
+        destination = "/home/ec2-user/eks_bastion_ssh.pem"
+        connection = {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = file("eks_bastion_ssh.pem")
+            host = self.map_public_ip
+
+        }
+    }
+    provisioner "remote-exec" {
+        inline = ["chmod 400 ~/eks_bastion_ssh.pem"]
+        connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "file(eks_bastion_ssh.pem)"
+            host = self.public.ip
+        }
 
     }
-
-provisioner "remote-exec" {
-    inline = ["chmod 400 ~/eks_bastion_ssh.pem"]
-    connection {
-        type = "ssh"
-        user = "ec2-user"
-        private_key = "file(eks_bastion_ssh.pem)"
-        host = self.public.ip
-    }
-
-}
 
 }
--- sg.tf
+++ sg.tf
@@ -10,8 +10,9 @@ resource "aws_security_group_rule" "public_in_ssh" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
-    
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.worker_group.id
+
 }
 
 resource "aws_security_group_rule" "public_in_https" {
@@ -20,7 +21,8 @@ resource "aws_security_group_rule" "public_in_https" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.worker_group.id
     
 }
 
@@ -31,5 +33,6 @@ resource "aws_security_group_rule" "public_out" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+    security_group_id = aws_security_group.worker_group.id
 
 }
