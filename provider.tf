96ba1accccecf6a11c6d13ba95af0e0f85e5e7f1
Berret Terry <berretterry@gmail.com>
2022-11-17 09:56:44 -0800

    upading credential passthrough

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
     region = "us-west-2"
-  
+    shared_config_files = "/Users/berretterry/.aws/credentials"
 }
--- vars.tf
+++ vars.tf
@@ -12,3 +12,5 @@ variable "azs" {
     type = list
     default = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
 }
+#variable "AWS_ACCESS_KEY" {}
+#variable "AWS_SECRET_KEY" {}
